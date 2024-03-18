15fbd9714f192fcf7d332ef0c83e5cc7ed168cdf
Heran76 <heran76@gmail.com>
2024-03-18 18:02:13 +0100

    añadir validar password

--- main.js
+++ main.js
@@ -1,89 +1,92 @@
 function validarUsuario(e){
-    let texto = e.value
-    let patron = /^[\w]{3,25}$/ 
+    let texto = e.value;
+    let patron = /^[\w]{3,25}$/;
     
-    e.nextElementSibling.innerHTML = patron.test(texto)
+    e.nextElementSibling.innerHTML = patron.test(texto);
 
     if(!patron.test(texto)){
-        document.getElementById("usuario").innerHTML=" Introduce un usuario entre 3 y 25 caracteres"
-        document.getElementById("usuario").style.color="red"
-    }else{
-        document.getElementById("usuario").innerHTML=" Usuario correcto"
-        document.getElementById("usuario").style.color="green"
+        document.getElementById("usuario").innerHTML = " Introduce un usuario entre 3 y 25 caracteres";
+        document.getElementById("usuario").style.color = "red";
+    } else {
+        document.getElementById("usuario").innerHTML = " Usuario correcto";
+        document.getElementById("usuario").style.color = "green";
     }
-    
 }
 
-//funcion contraseña
-
- function validarConstraseña(e){
+function validarConstraseña(e){
     let texto = e.value;
-    let patron =  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/
-    e.nextElementSibling.innerHTML = patron.test(texto)
+    let patron = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+
+    e.nextElementSibling.innerHTML = patron.test(texto);
+
     if(!patron.test(texto)){
-        document.getElementById("contraseña").innerHTML=" Introduce : Al menos 8 caracteres y una letra en mayúsculas, una minúscula y un digito "
-        document.getElementById("contraseña").style.color="red"
-    }else{
-        document.getElementById("contraseña").innerHTML=" Usuario correcto"
-        document.getElementById("contraseña").style.color="green"
+        document.getElementById("contraseña").innerHTML = " Introduce: Al menos 8 caracteres y una letra en mayúsculas, una minúscula y un dígito";
+        document.getElementById("contraseña").style.color = "red";
+    } else {
+        document.getElementById("contraseña").innerHTML = " Contraseña correcta";
+        document.getElementById("contraseña").style.color = "green";
     }
- }
+}
 
+function validarConfirmarConstraseña() {
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+    let message = document.getElementById('confirmarContraseña');
 
- //funcion password
+    if (password !== confirmPassword) {
+        message.innerHTML = '¡Error! Las contraseñas no coinciden.';
+        message.style.color = 'red';
+    } else {
+        message.innerHTML = '¡Contraseña confirmada correctamente!';
+        message.style.color = 'green';
+    }
+}
 
- function validarEmail(e){
+function validarEmail(e){
     let texto = e.value;
-    let patron = /^[\w\-\.]*@([\w-]+\.)+[\w-]{2,}$/
-    e.nextElementSibling.innerHTML = patron.test(texto)
+    let patron = /^[\w\-\.]*@([\w-]+\.)+[\w-]{2,}$/;
+    e.nextElementSibling.innerHTML = patron.test(texto);
+
     if(!patron.test(texto)){
-        document.getElementById("email").innerHTML=" Email y inconrrecto "
-        document.getElementById("email").style.color="red"
+        document.getElementById("email").innerHTML=" Email incorrecto ";
+        document.getElementById("email").style.color="red";
     }else{
-        document.getElementById("email").innerHTML="Email correcto"
-        document.getElementById("email").style.color="green"
-
+        document.getElementById("email").innerHTML="Email correcto";
+        document.getElementById("email").style.color="green";
     }
- }
+}
 
- //funcion nombre
-
- function validarNombre(e){
-
+function validarNombre(e){
     let texto = e.value;
-    let patron = /^[\D]{3,}$/
-    e.nextElementSibling.innerHTML = patron.test(texto)
+    let patron = /^[\D]{3,}$/;
+    e.nextElementSibling.innerHTML = patron.test(texto);
+
     if(!patron.test(texto)){
-        document.getElementById("nombre").innerHTML="Nombre incorrecto"
-        document.getElementById("nombre").style.color="red"
-        span.style.color="red"
+        document.getElementById("nombre").innerHTML="Nombre incorrecto";
+        document.getElementById("nombre").style.color="red";
     }else{
-        document.getElementById("nombre").innerHTML="Nombre  correcto"
-        document.getElementById("nombre").style.color="green"
+        document.getElementById("nombre").innerHTML="Nombre correcto";
+        document.getElementById("nombre").style.color="green";
     }
+}
 
- }
-
- function validarApellidos(e){
+function validarApellidos(e){
     let texto = e.value;
-    let patron = /^[\D]{2,} [\D]{2,}$/
-    e.nextElementSibling.innerHTML = patron.test(texto)
+    let patron = /^[\D]{2,} [\D]{2,}$/;
+    e.nextElementSibling.innerHTML = patron.test(texto);
+
     if(!patron.test(texto)){
-        document.getElementById("apellido").innerHTML=" Apellido incorrecto"
-        document.getElementById("apellido").style.color="red"
-        span.style.color="red"
+        document.getElementById("apellido").innerHTML="Apellido incorrecto";
+        document.getElementById("apellido").style.color="red";
     }else{
-        document.getElementById("apellido").innerHTML=" Apellido  correcto"
-        document.getElementById("apellido").style.color="green"
+        document.getElementById("apellido").innerHTML="Apellido correcto";
+        document.getElementById("apellido").style.color="green";
     }
+}
 
- }
-
- function validarCP() {
+function validarCP() {
     let cpValue = document.querySelector("textarea").value.split("\n");
     let patron = /^[0-9]{5}$/; 
-    
-    console.log(cpValue);
     
     if (!patron.test(cpValue[2])) { 
         alert('El código postal es incorrecto: ' + cpValue[2]);
@@ -93,6 +96,7 @@ function validarUsuario(e){
         `);
     }
 }
+
 
 
     
